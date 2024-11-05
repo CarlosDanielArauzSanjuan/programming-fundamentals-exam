ce7162656eced00ddff57923ba01e1d669e86ff7
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-11-05 12:44:06 -0500

    Solution exercise 2

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-# Ejercicios por realizar
-#1. Desarrolle el código fuente de un programa que permita ingresar cinco voltajes, 
-# obtener su promedio y visualizar `"ALTO VOLTAJE"`, si su promedio es mayor a 220, caso contrario sea menor mostrar `"VOLTAJE CORRECTO"`.
-   
-print ("CALCULAR VOLTAJE")
-n1 =float(input("ingrese el voltaje 1  :"))
-n2 =float(input("ingrese el voltaje 2  :"))
-n3 = float(input("ingrese el voltaje 3  :"))
-n4 = float(input("ingrese el voltaje 4  :"))
-n5 = float(input("ingrese el voltaje 5  :"))
-vol= (n1+n2+n3+n4+n5)/5
-if vol>220 :
-    print(" ALTO VOLTAJE")
-else  :
-    print("VOLTAJE CORRECTO")
+# 2. Desarrolle el código fuente de un programa que permita calcular el área de un triángulo equilátero, 
+# adicional visualizar `"DATOS NO VÁLIDOS"`, si el área es mayor a 1000.
+#   La fórmula para calcular el área `A` de un triángulo equilátero de lado `a` es: ( raizQUAD(3) / 4 )* (a**2)
+
+import math # call math fuction
+
+
+base = float (input("ingrese la medida del lado 'a' del triangulo  :"))
+area = ((math.sqrt(3) / 4 ) * math.pow(base,2))
+if area > 1000:
+    print(f""" 
+    DATOS NO VÁLIDOS
+    """)
+else: 
+    print(f""" 
+    el area del triangulo es :{area}
+    """)
